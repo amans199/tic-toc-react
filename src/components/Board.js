78cdd6da18e1ab0b2ea427141450225290c9162c
amans199 <ahmed.ouda1997@gmail.com>
2021-09-03 17:50:58 +0200

    adding initial state

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,17 +1,46 @@
 import Square from './Square'
+import React, { useState } from 'react'
 
 const Board = () => {
 
-    const actionHandler = (e) => {
-        console.log(e)
+    const [Board, setBoard] = useState(Array(9).fill(null))
+    const [xIsNext, setXIsNext] = useState(false)
+
+
+    const setBoardHandler = (position) => {
+        if (Board[position]) return
+
+        setBoard(
+            Board.map((square, index) => {
+                if (index === position) {
+                    return xIsNext ? 'X' : 'O'
+                }
+                return square
+            }
+            )
+        )
+
+        setXIsNext(!xIsNext)
     }
 
     return (
-        <>
-            <Square value={1} onClick={() => { actionHandler(22) }} />
-            <Square value={2} >chdiksldld</Square>
-            <Square value={3} />
-        </>
+        <main className="board">
+            <div className="row">
+                <Square value={Board[0]} onClick={() => { setBoardHandler(0) }} />
+                <Square value={Board[1]} onClick={() => { setBoardHandler(1) }} />
+                <Square value={Board[2]} onClick={() => { setBoardHandler(2) }} />
+            </div>
+            <div className="row">
+                <Square value={Board[3]} onClick={() => { setBoardHandler(3) }} />
+                <Square value={Board[4]} onClick={() => { setBoardHandler(4) }} />
+                <Square value={Board[5]} onClick={() => { setBoardHandler(5) }} />
+            </div>
+            <div className="row">
+                <Square value={Board[6]} onClick={() => { setBoardHandler(6) }} />
+                <Square value={Board[7]} onClick={() => { setBoardHandler(7) }} />
+                <Square value={Board[8]} onClick={() => { setBoardHandler(8) }} />
+            </div>
+        </main>
     )
 }
 
--- src/components/Square.js
+++ src/components/Square.js
@@ -3,7 +3,7 @@
 const Square = (props) => {
     return (
         <button type="button" onClick={props.onClick}>
-            {props.children}
+            {props.value}
         </button>
     )
 }
